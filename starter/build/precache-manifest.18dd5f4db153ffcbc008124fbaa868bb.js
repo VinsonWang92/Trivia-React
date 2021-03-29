a5a81cdd8cd8b724ec4266e0d419e24fdf7cc65a
VinsonWang92 <vinsonwang92@gmail.com>
2021-03-29 14:17:24 +1100

    added build folder

---
+++ starter/build/precache-manifest.18dd5f4db153ffcbc008124fbaa868bb.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed782ab7c26654b85792dc79de041e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fa3d790cec0cbe10cf4",
+    "url": "/static/css/main.ceee4cbb.chunk.css"
+  },
+  {
+    "revision": "2febfe7508a4c11616ed",
+    "url": "/static/js/2.c7fcaac6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c7fcaac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa3d790cec0cbe10cf4",
+    "url": "/static/js/main.53790c5f.chunk.js"
+  },
+  {
+    "revision": "69bbd7c1381a265f29c9",
+    "url": "/static/js/runtime-main.fda65720.js"
+  },
+  {
+    "revision": "30c182f739aff663bcfff73da82532c2",
+    "url": "/static/media/1.30c182f7.JPG"
+  },
+  {
+    "revision": "0ea8768f9aa2b71193b3046eee41d00b",
+    "url": "/static/media/14.0ea8768f.JPG"
+  },
+  {
+    "revision": "4f33b8ef446d47c3f48c604cafbb33d9",
+    "url": "/static/media/15.4f33b8ef.JPG"
+  },
+  {
+    "revision": "c8054bb50630511c0f3e0c0eb6a97a72",
+    "url": "/static/media/16.c8054bb5.JPG"
+  },
+  {
+    "revision": "36d215c20006bfbfaf2a4386c9f6c48e",
+    "url": "/static/media/18.36d215c2.jpg"
+  },
+  {
+    "revision": "9b8f1cbde0399d000c430b2e52e25282",
+    "url": "/static/media/19.9b8f1cbd.jpg"
+  },
+  {
+    "revision": "baa4c970b3290d0a7f6635af1527b107",
+    "url": "/static/media/2.baa4c970.JPG"
+  },
+  {
+    "revision": "d28be185bf0a26cc2c8f649b33c765cc",
+    "url": "/static/media/26.d28be185.jpg"
+  },
+  {
+    "revision": "ddf7343b06cc8080f78bd98768e110f9",
+    "url": "/static/media/3.ddf7343b.JPG"
+  },
+  {
+    "revision": "95c69584b04b2fad038de1fa5fcd8616",
+    "url": "/static/media/6.95c69584.JPG"
+  },
+  {
+    "revision": "de6f0ae8bd72a672d666afe8599b76d4",
+    "url": "/static/media/8.de6f0ae8.JPG"
+  },
+  {
+    "revision": "436b3879a071801f236ad605c9f23238",
+    "url": "/static/media/9.436b3879.JPG"
+  }
+]);
